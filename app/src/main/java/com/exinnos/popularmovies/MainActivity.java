1aba247a70e67ca5e36b20757e3090b3922bd648
RAMPRASAD <ramtrg@gmail.com>
2016-02-04 00:12:38 +0530

    Added Toolbar

--- app/src/main/java/com/exinnos/popularmovies/MainActivity.java
+++ app/src/main/java/com/exinnos/popularmovies/MainActivity.java
@@ -1,6 +1,7 @@
 package com.exinnos.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
     }
 }
