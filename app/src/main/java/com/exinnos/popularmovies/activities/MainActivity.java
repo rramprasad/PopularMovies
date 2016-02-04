fe6c87810f6f9cfae4bafd38d2ccb099c5a7d356
RAMPRASAD <ramtrg@gmail.com>
2016-02-04 23:51:52 +0530

    Added Movies fragment and container for single pane and two layouts.

--- app/src/main/java/com/exinnos/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/exinnos/popularmovies/activities/MainActivity.java
@@ -1,17 +1,34 @@
-package com.exinnos.popularmovies;
+package com.exinnos.popularmovies.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 
+import com.exinnos.popularmovies.R;
+
+/**
+ * @author RAMPRASAD
+ * Main Activity for movies.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private boolean twoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set toolbar
         Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolBar);
+
+        /**
+         * Check detail container available or not.
+         * It contains only sw600dp devices(tablets).
+         */
+        if(findViewById(R.id.framelayout_detail_container) != null){
+            twoPane = true;
+        }
     }
 }
