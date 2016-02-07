467b8d7717a1244ca54d0a575a5bb897fd2df194
RAMPRASAD <ramtrg@gmail.com>
2016-02-07 21:27:24 +0530

    Added movies fragment layout and integrated with TMDB api and loaded images.

--- app/src/main/java/com/exinnos/popularmovies/fragments/MoviesFragment.java
+++ app/src/main/java/com/exinnos/popularmovies/fragments/MoviesFragment.java
@@ -1,13 +1,35 @@
 package com.exinnos.popularmovies.fragments;
 
 import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.exinnos.popularmovies.BuildConfig;
 import com.exinnos.popularmovies.R;
+import com.exinnos.popularmovies.adapters.MoviesAdapter;
+import com.exinnos.popularmovies.data.Movie;
+import com.exinnos.popularmovies.util.AppConstants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * @author RAMPRASAD
@@ -21,6 +43,9 @@ public class MoviesFragment extends Fragment {
     private OnMoviesFragmentListener mListener;
     private View rootView;
     private RecyclerView moviesRecyclerView;
+    private GridLayoutManager moviesGridLayoutManager;
+    private ArrayList<Movie> moviesArrayList;
+    private MoviesAdapter moviesAdapter;
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -50,7 +75,22 @@ public class MoviesFragment extends Fragment {
 
         moviesRecyclerView.setHasFixedSize(true);
 
+        moviesGridLayoutManager = new GridLayoutManager(getActivity(), 2);
+
+        moviesRecyclerView.setLayoutManager(moviesGridLayoutManager);
+
+        moviesArrayList = new ArrayList<>();
+        moviesAdapter = new MoviesAdapter(getActivity(),moviesArrayList);
+
+        moviesRecyclerView.setAdapter(moviesAdapter);
+
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        new FetchMoviesAsyncTask().execute("popularity.desc");
     }
 
     @Override
@@ -75,5 +115,151 @@ public class MoviesFragment extends Fragment {
      */
     public interface OnMoviesFragmentListener {
         void onMoviesFragmentItemSelected();
+    }
+
+    private class FetchMoviesAsyncTask extends AsyncTask<String, Void, Movie[]> {
+
+        private final String LOG_TAG = FetchMoviesAsyncTask.class.getSimpleName();
+
+        @Override
+        protected Movie[] doInBackground(String... params) {
+            // If no params given,return.
+            if(params.length == 0){
+                return null;
+            }
+
+            HttpURLConnection httpURLConnection = null;
+            BufferedReader bufferedReader = null;
+            String responseJSONString = null;
+
+            try {
+                final String SORY_BY_PARAM = "sort_by";
+                final String API_KEY_PARAM = "api_key";
+
+                Uri.Builder uriBuilder = Uri.parse(AppConstants.MOVIES_BASE_URL).buildUpon();
+                uriBuilder.appendQueryParameter(SORY_BY_PARAM,params[0]);
+                uriBuilder.appendQueryParameter(API_KEY_PARAM, BuildConfig.THE_MOVIE_DB_API_KEY);
+                Uri uri = uriBuilder.build();
+
+                URL url = new URL(uri.toString());
+
+                //Open the connection
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.connect();
+
+
+                // Read response from input stream.
+                InputStream inputStream = httpURLConnection.getInputStream();
+
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                StringBuffer responseStringBuffer = new StringBuffer();
+
+                String responseLine;
+                while ((responseLine=bufferedReader.readLine()) != null){
+                    responseStringBuffer.append(responseLine);
+                }
+
+                // If no response available,return.
+                if(responseStringBuffer.length() == 0){
+                    return null;
+                }
+
+                // Response json string
+                responseJSONString = responseStringBuffer.toString();
+
+                Movie[] movies = parseMoviesJSON(responseJSONString);
+
+                return movies;
+
+            } catch (MalformedURLException e) {
+                Log.e(LOG_TAG,"MalformedURLException on URL",e);
+                return null;
+            } catch (IOException e) {
+                Log.e(LOG_TAG,"Error",e);
+                return null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e(LOG_TAG,"Json parse error",e);
+                return null;
+            } finally {
+
+                if(httpURLConnection != null){
+                    httpURLConnection.disconnect();
+                }
+
+                if(bufferedReader != null){
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG,"exception while closing buffered reader",e);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Movie[] moviesArray) {
+            super.onPostExecute(moviesArray);
+
+            if(moviesArray != null){
+
+                moviesArrayList.clear();
+                for(Movie movie : moviesArray){
+                    moviesArrayList.add(movie);
+                }
+
+                moviesAdapter.notifyDataSetChanged();
+            }
+
+        }
+
+        /**
+         * Parse given response JSON String.
+         * @param responseJSONString
+         * @throws JSONException
+         */
+        private Movie[] parseMoviesJSON(String responseJSONString) throws JSONException{
+
+            final String KEY_RESULTS = "results";
+            final String KEY_POSTER_PATH = "poster_path";
+            final String KEY_OVERVIEW = "overview";
+            final String KEY_RELEASE_DATE = "release_date";
+            final String KEY_ID = "id";
+            final String KEY_TITLE = "original_title";
+            final String KEY_VOTE_AVERAGE = "vote_average";
+
+
+            JSONObject moviesJsonObject = new JSONObject(responseJSONString);
+
+            JSONArray moviesResultsJSONArray = moviesJsonObject.getJSONArray(KEY_RESULTS);
+
+            int moviesLength = moviesResultsJSONArray.length();
+
+            Movie[] moviesArray = new Movie[moviesLength];
+
+            for (int i=0; i < moviesResultsJSONArray.length(); i++){
+                JSONObject movieJsonObject = moviesResultsJSONArray.getJSONObject(i);
+                String posterPath = movieJsonObject.getString(KEY_POSTER_PATH);
+                String overView = movieJsonObject.getString(KEY_OVERVIEW);
+                String releaseDate = movieJsonObject.getString(KEY_RELEASE_DATE);
+                int movieId = movieJsonObject.getInt(KEY_ID);
+                String keyTitle = movieJsonObject.getString(KEY_TITLE);
+                double voteAverage = movieJsonObject.getDouble(KEY_VOTE_AVERAGE);
+
+                Movie movie = new Movie();
+                movie.setPosterPath(posterPath);
+                movie.setOverView(overView);
+                movie.setReleaseDate(releaseDate);
+                movie.setMovieId(movieId);
+                movie.setTitle(keyTitle);
+                movie.setVoteAverage(voteAverage);
+
+                moviesArray[i] = movie;
+            }
+
+            return moviesArray;
+        }
     }
 }
---
+++ app/src/main/java/com/exinnos/popularmovies/util/AppConstants.java
@@ -0,0 +1,11 @@
+package com.exinnos.popularmovies.util;
+
+/**
+ * Created by RAMPRASAD on 2/7/2016.
+ */
+public class AppConstants {
+
+
+    public static final String MOVIES_BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+    public static final String MOVIE_POSTER_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185";
+}
