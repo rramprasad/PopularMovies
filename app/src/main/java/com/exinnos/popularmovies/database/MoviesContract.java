87cd05aacff01f580c1c5f82b50e77588e403615
RAMPRASAD <ramtrg@gmail.com>
2016-03-26 21:55:22 +0530

    Sync Popular and highest rated movies to local database.

--- app/src/main/java/com/exinnos/popularmovies/database/MoviesContract.java
+++ app/src/main/java/com/exinnos/popularmovies/database/MoviesContract.java
@@ -1,5 +1,7 @@
 package com.exinnos.popularmovies.database;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -44,6 +46,10 @@ public final class MoviesContract {
         // Add Movies table path on top of base content URI
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
 
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIES;
+
         public static final String TABLE_NAME = "movies";
 
         public static final String COLUMN_ADULT = "adult";
@@ -64,14 +70,24 @@ public final class MoviesContract {
         public static final String COLUMN_VIDEO = "video";
         public static final String COLUMN_VOTE_AVERAGE = "vote_average";
         public static final String COLUMN_VOTE_COUNT = "vote_count";
+
+        public static Uri buildMoviesUri() {
+            return CONTENT_URI.buildUpon().build();
+        }
+
+        public static Uri buildMoviesWithIdUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI,id);
+        }
     }
 
     // Table for popular movies
     public static final class PopularMoviesEntry implements BaseColumns {
 
-
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_POPULAR_MOVIES).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_POPULAR_MOVIES;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_POPULAR_MOVIES;
 
         public static final String TABLE_NAME = "popular_movies";
 
@@ -79,6 +95,10 @@ public final class MoviesContract {
 
         public static Uri buildPopularMoviesUri() {
             return CONTENT_URI.buildUpon().build();
+        }
+
+        public static Uri buildPopularMoviesWithIdUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI,id);
         }
     }
 
@@ -90,12 +110,20 @@ public final class MoviesContract {
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_HIGHEST_RATED_MOVIES).build();
 
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_HIGHEST_RATED_MOVIES;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_HIGHEST_RATED_MOVIES;
+
+
         public static final String TABLE_NAME = "highest_rated_movies";
 
         public static final String COLUMN_MOVIE_ID = "movie_id";
 
         public static Uri buildHighestRatedMoviesUri() {
             return CONTENT_URI.buildUpon().build();
+        }
+
+        public static Uri buildHighestRatedMoviesWithIdUri(Integer id) {
+            return ContentUris.withAppendedId(CONTENT_URI,id);
         }
     }
 
@@ -106,6 +134,10 @@ public final class MoviesContract {
 
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_FAVORITE_MOVIES).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FAVORITE_MOVIES;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FAVORITE_MOVIES;
+
 
         public static final String TABLE_NAME = "favorite_movies";
 
@@ -123,6 +155,10 @@ public final class MoviesContract {
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_MOVIE_REVIEWS).build();
 
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE_REVIEWS;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE_REVIEWS;
+
+
         public static final String TABLE_NAME = "movie_reviews";
 
         public static final String COLUMN_MOVIE_ID = "movie_id";
@@ -135,6 +171,10 @@ public final class MoviesContract {
 
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_MOVIE_TRAILERS).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE_TRAILERS;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE_TRAILERS;
+
 
         public static final String TABLE_NAME = "movie_trailers";
 
