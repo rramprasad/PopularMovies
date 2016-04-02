5ae4f917432f8508eea82063ad90adcda9ee2927
RAMPRASAD <ramtrg@gmail.com>
2016-04-03 00:01:31 +0530

    Updated Movies list to Cardview. Added viewpager for movie details. Added favorite FAB button.

--- app/src/main/java/com/exinnos/popularmovies/adapters/MoviesAdapter.java
+++ app/src/main/java/com/exinnos/popularmovies/adapters/MoviesAdapter.java
@@ -35,7 +35,7 @@ public class MoviesAdapter extends CursorRecyclerViewAdapter<MoviesAdapter.Movie
     @Override
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_grid_item, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_grid_item,parent,false);
 
         MovieViewHolder movieViewHolder = new MovieViewHolder(view);
         return movieViewHolder;
@@ -77,11 +77,11 @@ public class MoviesAdapter extends CursorRecyclerViewAdapter<MoviesAdapter.Movie
 
             Cursor cursor = getCursor();
 
-            int movieId = 0;
-            if (cursor != null) {
-                movieId = cursor.getInt(cursor.getColumnIndex(MoviesContract.MoviesEntry.COLUMN_POSTER_PATH));
+            if (cursor != null && cursor.getCount() > 0) {
+                cursor.moveToPosition(getAdapterPosition());
+                int movieId = cursor.getInt(cursor.getColumnIndex(MoviesContract.PopularMoviesEntry.COLUMN_MOVIE_ID));
+                mMovieClickListener.onMovieClicked(movieId);
             }
-            mMovieClickListener.onMovieClicked(movieId);
         }
     }
 }
