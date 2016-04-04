64532eddf5676e8c3065dc6a9f249eb3ea349602
RAMPRASAD <ramtrg@gmail.com>
2016-04-05 00:10:24 +0530

    Added code to play trailers on youtube. Added tabs on movie details viewpager. Modified app color

--- app/src/main/java/com/exinnos/popularmovies/adapters/MovieDetailsPagerAdapter.java
+++ app/src/main/java/com/exinnos/popularmovies/adapters/MovieDetailsPagerAdapter.java
@@ -10,6 +10,9 @@ import com.exinnos.popularmovies.fragments.MovieReviewsFragment;
 import com.exinnos.popularmovies.fragments.MovieSummaryFragment;
 import com.exinnos.popularmovies.fragments.MovieTrailersFragment;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by RAMPRASAD on 4/2/2016.
  * Adapter for movie details viewpager
@@ -18,10 +21,12 @@ public class MovieDetailsPagerAdapter extends FragmentPagerAdapter {
 
     private static final int FRAGMENT_PAGES_COUNT = 3;
     private int mMovieId;
+    private List<String> mFragmentTitleList;
 
-    public MovieDetailsPagerAdapter(FragmentManager supportFragmentManager,int movieId) {
+    public MovieDetailsPagerAdapter(FragmentManager supportFragmentManager,int movieId,List<String> fragmentTitleList) {
         super(supportFragmentManager);
         this.mMovieId = movieId;
+        this.mFragmentTitleList = fragmentTitleList;
     }
 
     @Override
@@ -42,4 +47,11 @@ public class MovieDetailsPagerAdapter extends FragmentPagerAdapter {
     public int getCount() {
         return FRAGMENT_PAGES_COUNT;
     }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+
 }
