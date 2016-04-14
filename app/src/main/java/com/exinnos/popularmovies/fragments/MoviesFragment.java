b05215b973662104cebf2d9bbde85e7750f28b97
RAMPRASAD <ramtrg@gmail.com>
2016-04-14 16:05:36 +0530

    Removed api key

--- app/src/main/java/com/exinnos/popularmovies/fragments/MoviesFragment.java
+++ app/src/main/java/com/exinnos/popularmovies/fragments/MoviesFragment.java
@@ -38,7 +38,6 @@ import com.exinnos.popularmovies.network.MoviesAPIService;
 import com.exinnos.popularmovies.sync.MoviesSyncAdapter;
 import com.exinnos.popularmovies.util.AppConstants;
 import com.exinnos.popularmovies.util.AppUtilities;
-import com.facebook.stetho.okhttp3.StethoInterceptor;
 
 import java.util.ArrayList;
 import java.util.List;
